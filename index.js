ce24ef1df3a1eee0841b8a34208ee98852313991
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-01-15 11:44:17 -0300

    fix: melhorando a responsividade

--- index.js
+++ index.js
@@ -1,49 +1,86 @@
-const botao = document.querySelector("#searchButton")
-const weatherBox = document.querySelector(".search-box")
-const weatherDetails = document.querySelector(".weather-details")
-const errorImage = document.querySelector("#image-error")
-const weatherImage = document.querySelector("#weather-condition")
+const botao = document.querySelector("#searchButton");
+const weatherBox = document.querySelector(".search-box");
+const weatherDetails = document.querySelector(".weather-details");
+const errorImage = document.querySelector("#image-error");
+const weatherImage = document.querySelector("#weather-condition");
+const loading = document.querySelector(".loading-text");
 
 botao.addEventListener("click", () => {
-    pegarDados()
-})
+    pegarDados();
+});
 
 async function pegarDados() {
     const cidade = document.querySelector("#inputText").value;
-    
+
+    weatherDetails.style.display = "none"
     weatherBox.classList.remove("search-box-expand", "search-box-error");
     weatherBox.classList.add("search-box-loading");
+    loading.style.display = "block";
     errorImage.style.display = "none";
 
     try {
-        const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=8ec509478dca5663906e942e56591d5d`);
+        const resposta = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=8ec509478dca5663906e942e56591d5d`
+        );
 
-        if(resposta.status !== 200){
-            weatherBox.classList.remove('search-box-loading');
-            weatherBox.classList.remove('search-box');
-            weatherBox.classList.add('search-box-expand')
+        if (resposta.status !== 200) {
+            weatherBox.classList.remove("search-box");
+            weatherBox.classList.add("search-box-expand");
+
+            loading.style.display = "none";
             errorImage.style.display = "block";
             return;
+        } else {
+            const dados = await resposta.json();
+            console.log(dados);
+
+            const weatherCondition = dados.weather[0].main;
+            const weatherCases = {
+                Clear: "./images/clear.png",
+                Clouds: "./images/cloud.png",
+                Mist: "./images/mist.png",
+                Rain: "./images/rain.png",
+                Snow: "./images/snow.png",
+            };
+
+            weatherImage.src = weatherCases[weatherCondition] || "./images/404.png";
+
+            const temperature = document.querySelector("#temp-num");
+            temperature.textContent = `${dados.main.temp} ºC`;
+
+            const humidity = document.querySelector("#hum-num");
+            humidity.textContent = `${dados.main.humidity}% UR`;
+
+            loading.style.display = "none";
+            weatherBox.classList.remove("search-box-loading", "search-box");
+            weatherBox.classList.add("search-box-expand");
+            weatherDetails.style.display = "block";
         }
-
-        const dados = await resposta.json();
-        console.log(dados)
-
-        const weatherCondition = dados.weather[0].main;
-        const weatherCases = {
-            Clear: "./images/clear.png",
-            Cloud: "./images/cloud.png",
-            Mist: "./images/mist.png",
-            Rain: "./images/rain.png",
-            Snow: "./images/snow.png"
-        }
-
-        weatherImage.src = weatherCases[weatherCondition] || ""
-
-        weatherBox.classList.remove('search-box-loading');
-        weatherBox.classList.add('search-box-expand')
-        weatherDetails.style.display = "block"
     } catch (erro) {
-        console.log("Erro: ", erro.message)
+        weatherBox.classList.remove("search-box");
+        weatherBox.classList.add("search-box-expand");
+        loading.style.display = "none";
+        errorImage.style.display = "block";
     }
 }
+
+function loadingText() {
+    const loadingText = "Loading data...";
+    const container = document.querySelector(".loading-text");
+
+    container.innerHTML = "";
+
+    loadingText.split("").forEach((char, index) => {
+        const span = document.createElement("span");
+        span.textContent = char;
+        span.style.setProperty('--i', index)
+        container.appendChild(span);
+    });
+
+    if (!weatherBox.contains(container)) {
+        weatherBox.appendChild(container);
+    }
+
+}
+
+loadingText();
